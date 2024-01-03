c8eaff67bfc8b2de6456bf0edf1e4ea9439daebb
5h1ngy <davide.scarano.73@gmail.com>
2024-01-03 04:37:16 +0100

    feat: peer dependencies

---
+++ lib/dragging.d.ts
@@ -0,0 +1,4 @@
+export function reorder(list: any, startIndex: number, endIndex: number): any;
+export function move(source: any, destination: any, droppableSource: any, droppableDestination: any): any;
+export function initItems(items: any[]): any[];
+export function initGroups(groups: any[]): any[];
---
+++ lib/index.d.ts
@@ -0,0 +1,14 @@
+export namespace dragging {
+    export { initGroups };
+    export { initItems };
+    export { move };
+    export { reorder };
+}
+export namespace utils {
+    export { generateRandomString };
+}
+import { initGroups } from './dragging';
+import { initItems } from './dragging';
+import { move } from './dragging';
+import { reorder } from './dragging';
+import { generateRandomString } from './utils';
